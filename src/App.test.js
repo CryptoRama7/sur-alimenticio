88ec25d4bf3131770c910db87f52dc6e06b65a5a
Ramiro Dubini <ramirodubini@gmail.com>
2022-08-30 23:41:41 -0300

    primeros-estilos

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Nav from './components/NavBar';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Nav />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import '../App.css'
+
+const Nav = () => {
+    return (
+    <header>
+        <nav>
+            <h1 class="titulo-nav">Sur Alimenticio</h1>
+            <a class="productos" href="">Productos</a>
+            <a class="sobre-nosotros" href="">Sobre Nosotros</a>
+            <a class="locales" href="">Locales</a>
+        </nav>
+    </header>
+    );
+}
+
+export default Nav;
